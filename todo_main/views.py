7e2d9f9407b0747056ee9ac7a86bc5dcda88fe0c
unknown <carlos.costa.pimentel@gmail.com>
2023-07-07 11:01:33 +0100

    added dynamic task display in html template

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,5 +3,7 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False)
-    print(tasks)
-    return render(request, 'home.html')
+    context = {
+        'tasks': tasks,
+        }
+    return render(request, 'home.html', context)
