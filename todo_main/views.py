67bde0c234ba2c11ecabb6bf816d6184e432f00c
unknown <carlos.costa.pimentel@gmail.com>
2023-07-07 12:25:07 +0100

    display the complete tasks in descending order

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    task_completed = Task.objects.filter(is_completed=True).order_by('-updated_at')
+
     context = {
         'tasks': tasks,
+        'task_completed': task_completed,
         }
     return render(request, 'home.html', context)
