28ff56a302caa779905b0dccddf424c4f99e58cb
unknown <carlos.costa.pimentel@gmail.com>
2023-07-07 11:12:56 +0100

    corrected typo on search_fields

--- todo/admin.py
+++ todo/admin.py
@@ -4,6 +4,6 @@ from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task', 'is_completed', 'updated_at')
-    search_fields = ('tasks',)
+    search_fields = ('task',)
 
 admin.site.register(Task, TaskAdmin)
